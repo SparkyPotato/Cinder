5fc07cf42d7a57fdefaa61845bad68cb12edff3b
SparkyPotato <noob.sparkypotato@gmail.com>
2020-11-25 17:53:06 +0530

    Direction, Point, and Normal

--- Cinder/Source/Math/Vector.cpp
+++ Cinder/Source/Math/Vector.cpp
@@ -13,7 +13,21 @@ Direction::Direction(float x, float y, float z)
 
 Direction::Direction(__m128 vector)
 	: m_Vector(vector)
-{}
+{
+	ASSERT(!IsNAN(), "Constructed direction is NaN!");
+}
+
+Direction::operator Normal()
+{
+	return Normal(X, Y, Z);
+}
+
+Direction& Direction::operator=(const Direction& other)
+{
+	m_Vector = other.m_Vector;
+
+	return *this;
+}
 
 float Direction::operator[](uint8_t index) const
 {
@@ -27,13 +41,6 @@ float& Direction::operator[](uint8_t index)
 	ASSERT(index < 3, "Direction index out of range!");
 
 	return reinterpret_cast<float*>(&m_Vector)[index];
-}
-
-Direction& Direction::operator=(const Direction& other)
-{
-	m_Vector = other.m_Vector;
-
-	return *this;
 }
 
 Direction Direction::operator+(const Direction& other) const
@@ -77,6 +84,11 @@ Direction Direction::operator*(float scalar) const
 	);
 }
 
+Direction operator*(float scale, const Direction& direction)
+{
+	return direction * scale;
+}
+
 Direction& Direction::operator*=(float scalar)
 { 
 	__m128 scale = _mm_set_ps(scalar, scalar, scalar, scalar);
@@ -101,36 +113,14 @@ Direction& Direction::operator/=(float scalar)
 	return *this;
 }
 
-float Direction::Dot(const Direction& first, const Direction& second)
-{
-	// _mm_dot_ps is not used to retain support for all x64 processors.
-	// It is also slower on Ryzen.
-	__m128 mul = _mm_mul_ps(first.m_Vector, second.m_Vector);
-	__m128 shuffle = _mm_shuffle_ps(mul, mul, _MM_SHUFFLE(2, 3, 0, 1));
-	__m128 sums = _mm_add_ps(mul, shuffle);
-	shuffle = _mm_movehl_ps(shuffle, sums);
-	sums = _mm_add_ss(sums, shuffle);
-	return _mm_cvtss_f32(sums);
-}
-
-Direction Direction::Cross(const Direction& first, const Direction& second)
-{
-	__m128 result = _mm_sub_ps(
-		_mm_mul_ps(second.m_Vector, _mm_shuffle_ps(first.m_Vector, first.m_Vector, _MM_SHUFFLE(3, 0, 2, 1))),
-		_mm_mul_ps(first.m_Vector, _mm_shuffle_ps(second.m_Vector, second.m_Vector, _MM_SHUFFLE(3, 0, 2, 1)))
-	);
-	
-	return Direction(_mm_shuffle_ps(result, result, _MM_SHUFFLE(3, 0, 2, 1 )));
-}
-
 float Direction::GetLengthSquare() const
 {
-	return Direction::Dot(*this, *this);
+	return Dot(*this, *this);
 }
 
 float Direction::GetLength() const
 {
-	return sqrt(Direction::Dot(*this, *this));
+	return std::sqrt(Dot(*this, *this));
 }
 
 Direction Direction::GetNormalized() const
@@ -148,4 +138,326 @@ Direction& Direction::Normalize()
 bool Direction::IsNAN()
 {
 	return std::isnan(X) || std::isnan(Y) || std::isnan(Z);
+}
+
+float Dot(const Direction& first, const Direction& second)
+{
+	// _mm_dot_ps is not used to retain support for all x64 processors.
+	// It is also slower on Ryzen.
+	__m128 mul = _mm_mul_ps(first.m_Vector, second.m_Vector);
+	__m128 shuffle = _mm_shuffle_ps(mul, mul, _MM_SHUFFLE(2, 3, 0, 1));
+	__m128 sums = _mm_add_ps(mul, shuffle);
+	shuffle = _mm_movehl_ps(shuffle, sums);
+	sums = _mm_add_ss(sums, shuffle);
+	return _mm_cvtss_f32(sums);
+}
+
+Direction Cross(const Direction& first, const Direction& second)
+{
+	__m128 result = _mm_sub_ps(
+		_mm_mul_ps(second.m_Vector, _mm_shuffle_ps(first.m_Vector, first.m_Vector, _MM_SHUFFLE(3, 0, 2, 1))),
+		_mm_mul_ps(first.m_Vector, _mm_shuffle_ps(second.m_Vector, second.m_Vector, _MM_SHUFFLE(3, 0, 2, 1)))
+	);
+
+	return Direction(_mm_shuffle_ps(result, result, _MM_SHUFFLE(3, 0, 2, 1)));
+}
+
+void GenerateCoordinateSystem(const Direction& normalized, Direction& outFirst, Direction& outSecond)
+{
+	if (std::abs(normalized.X) > std::abs(normalized.Y))
+	{
+		outFirst = Direction(-normalized.Z, 0, normalized.X) /
+			std::sqrt(normalized.X * normalized.X + normalized.Z * normalized.Z);
+	}
+	else
+	{
+		outSecond = Direction(0, normalized.Z, -normalized.Y) /
+			std::sqrt(normalized.Y * normalized.Y + normalized.Z * normalized.Z);
+	}
+	outSecond = Cross(normalized, outFirst);
+}
+
+Direction Shuffle(const Direction& direction, uint8_t x, uint8_t y, uint8_t z)
+{
+	return Direction(direction[x], direction[y], direction[z]);
+}
+
+Point::Point(float x, float y, float z)
+{
+	m_Vector = _mm_set_ps(1.f, z, y, x);
+
+	ASSERT(!IsNAN(), "Constructed point is NaN!");
+}
+
+Point::Point(__m128 vector)
+	: m_Vector(vector)
+{
+	ASSERT(!IsNAN(), "Constructed point is NaN!");
+}
+
+Point& Point::operator=(const Point& other)
+{
+	m_Vector = other.m_Vector;
+
+	return *this;
+}
+
+float Point::operator[](uint8_t index) const
+{
+	ASSERT(index < 3, "Point index out of range!");
+
+	return reinterpret_cast<const float*>(&m_Vector)[index];
+}
+
+float& Point::operator[](uint8_t index)
+{
+	ASSERT(index < 3, "Point index out of range!");
+
+	return reinterpret_cast<float*>(&m_Vector)[index];
+}
+
+Point Point::operator+(const Direction& direction) const
+{
+	return Point(
+		_mm_add_ps(m_Vector, direction.m_Vector)
+	);
+}
+
+Point& Point::operator+=(const Direction& direction)
+{
+	m_Vector = _mm_add_ps(m_Vector, direction.m_Vector);
+
+	return *this;
+}
+
+Direction Point::operator-(const Point& other) const
+{
+	return Direction(
+		_mm_sub_ps(m_Vector, other.m_Vector)
+	);
+}
+
+Point Point::operator-(const Direction& direction) const
+{
+	return Point(
+		_mm_sub_ps(m_Vector, direction.m_Vector)
+	);
+}
+
+Point& Point::operator-=(const Direction& direction)
+{
+	m_Vector = _mm_sub_ps(m_Vector, direction.m_Vector);
+
+	return *this;
+}
+
+float Distance(const Point& first, const Point& second)
+{
+	return (first - second).GetLength();
+}
+
+float DistanceSquare(const Point& first, const Point& second)
+{
+	return (first - second).GetLengthSquare();
+}
+
+Point Lerp(const Point& from, const Point& to, float value)
+{
+	return Point(
+		from + (to - from) * value
+	);
+}
+
+Point Min(const Point& first, const Point& second)
+{
+	return Point(
+		std::min(first.X, second.X),
+		std::min(first.Y, second.Y),
+		std::min(first.Z, second.Z)
+	);
+}
+
+Point Max(const Point& first, const Point& second)
+{
+	return Point(
+		std::max(first.X, second.X),
+		std::max(first.Y, second.Y),
+		std::max(first.Z, second.Z)
+	);
+}
+
+bool Point::IsNAN()
+{
+	return std::isnan(X) || std::isnan(Y) || std::isnan(Z);
+}
+
+Point Shuffle(const Point& point, uint8_t x, uint8_t y, uint8_t z)
+{
+	return Point(point[x], point[y], point[z]);
+}
+
+Normal::Normal(float x, float y, float z)
+{
+	m_Vector = _mm_set_ps(0.f, z, y, x);
+
+	ASSERT(!IsNAN(), "Constructed normal is NaN!");
+}
+
+Normal::Normal(__m128 vector)
+	: m_Vector(vector)
+{
+	ASSERT(!IsNAN(), "Constructed normal is NaN!");
+}
+
+Normal::operator Direction()
+{
+	return Direction(X, Y, Z);
+}
+
+Normal& Normal::operator=(const Normal& other)
+{
+	m_Vector = other.m_Vector;
+
+	return *this;
+}
+
+float Normal::operator[](uint8_t index) const
+{
+	ASSERT(index < 3, "Normal index out of range!");
+
+	return reinterpret_cast<const float*>(&m_Vector)[index];
+}
+
+float& Normal::operator[](uint8_t index)
+{
+	ASSERT(index < 3, "Normal index out of range!");
+
+	return reinterpret_cast<float*>(&m_Vector)[index];
+}
+
+Normal Normal::operator+(const Normal& other) const
+{
+	return Normal(
+		_mm_add_ps(m_Vector, other.m_Vector)
+	);
+}
+
+Normal& Normal::operator+=(const Normal& other)
+{
+	m_Vector = _mm_add_ps(m_Vector, other.m_Vector);
+
+	return *this;
+}
+
+Normal Normal::operator-() const
+{
+	return Normal(-X, -Y, -Z);
+}
+
+Normal Normal::operator-(const Normal& other) const
+{
+	return Normal(
+		_mm_sub_ps(m_Vector, other.m_Vector)
+	);
+}
+
+Normal& Normal::operator-=(const Normal& other)
+{
+	m_Vector = _mm_sub_ps(m_Vector, other.m_Vector);
+
+	return *this;
+}
+
+Normal Normal::operator*(float scalar) const
+{
+	__m128 scale = _mm_set_ps(scalar, scalar, scalar, scalar);
+	return Normal(
+		_mm_mul_ps(m_Vector, scale)
+	);
+}
+
+Normal& Normal::operator*=(float scalar)
+{
+	__m128 scale = _mm_set_ps(scalar, scalar, scalar, scalar);
+	m_Vector = _mm_mul_ps(m_Vector, scale);
+
+	return *this;
+}
+
+Normal Normal::operator/(float scalar) const
+{
+	__m128 scale = _mm_set_ps(scalar, scalar, scalar, scalar);
+	return Normal(
+		_mm_div_ps(m_Vector, scale)
+	);
+}
+
+Normal& Normal::operator/=(float scalar)
+{
+	__m128 scale = _mm_set_ps(scalar, scalar, scalar, scalar);
+	m_Vector = _mm_div_ps(m_Vector, scale);
+
+	return *this;
+}
+
+float Normal::GetLengthSquare() const
+{
+	return Dot(*this, *this);
+}
+
+float Normal::GetLength() const
+{
+	return std::sqrt(Dot(*this, *this));
+}
+
+Normal Normal::GetNormalized() const
+{
+	return *this / GetLength();
+}
+
+Normal& Normal::Normalize()
+{
+	*this /= GetLength();
+
+	return *this;
+}
+
+bool Normal::IsNAN()
+{
+	return std::isnan(X) || std::isnan(Y) || std::isnan(Z);
+}
+
+float Dot(const Normal& first, const Normal& second)
+{
+	__m128 mul = _mm_mul_ps(first.m_Vector, second.m_Vector);
+	__m128 shuffle = _mm_shuffle_ps(mul, mul, _MM_SHUFFLE(2, 3, 0, 1));
+	__m128 sums = _mm_add_ps(mul, shuffle);
+	shuffle = _mm_movehl_ps(shuffle, sums);
+	sums = _mm_add_ss(sums, shuffle);
+	return _mm_cvtss_f32(sums);
+}
+
+float Dot(const Normal& first, const Direction& second)
+{
+	__m128 mul = _mm_mul_ps(first.m_Vector, second.m_Vector);
+	__m128 shuffle = _mm_shuffle_ps(mul, mul, _MM_SHUFFLE(2, 3, 0, 1));
+	__m128 sums = _mm_add_ps(mul, shuffle);
+	shuffle = _mm_movehl_ps(shuffle, sums);
+	sums = _mm_add_ss(sums, shuffle);
+	return _mm_cvtss_f32(sums);
+}
+
+float Dot(const Direction& first, const Normal& second)
+{
+	__m128 mul = _mm_mul_ps(first.m_Vector, second.m_Vector);
+	__m128 shuffle = _mm_shuffle_ps(mul, mul, _MM_SHUFFLE(2, 3, 0, 1));
+	__m128 sums = _mm_add_ps(mul, shuffle);
+	shuffle = _mm_movehl_ps(shuffle, sums);
+	sums = _mm_add_ss(sums, shuffle);
+	return _mm_cvtss_f32(sums);
+}
+
+Normal FlipAlong(const Normal& normal, const Direction& along)
+{
+	return (Dot(normal, along) < 0.f) ? -normal : normal;
 }
--- Cinder/Source/Math/Vector.h
+++ Cinder/Source/Math/Vector.h
@@ -3,6 +3,8 @@
 // Portable SIMD intrinsics
 #include <immintrin.h>
 
+class Normal;
+
 // Direction in 3D space, has a W coordinate of 0
 class Direction
 {
@@ -10,11 +12,12 @@ public:
 	Direction() = default;
 	Direction(float x, float y, float z);
 	Direction(const Direction& other) = default;
+	explicit operator Normal();
+
+	Direction& operator=(const Direction& other);
 
 	float operator[](uint8_t index) const;
 	float& operator[](uint8_t index);
-
-	Direction& operator=(const Direction& other);
 
 	Direction operator+(const Direction& other) const;
 	Direction& operator+=(const Direction& other);
@@ -28,10 +31,6 @@ public:
 	
 	Direction operator/(float scalar) const;
 	Direction& operator/=(float scalar);
-	
-	static float Dot(const Direction& first, const Direction& second);
-	// Only calculates a 3D cross product
-	static Direction Cross(const Direction& first, const Direction& second);
 	
 	float GetLengthSquare() const;
 	float GetLength() const;
@@ -47,7 +46,115 @@ public:
 	float& Z = reinterpret_cast<float*>(&m_Vector)[2];
 
 private:
+	friend class Point;
+	friend float Dot(const Direction& first, const Direction& second);
+	friend float Dot(const Normal& first, const Direction& second);
+	friend float Dot(const Direction& first, const Normal& second);
+	friend Direction Cross(const Direction& first, const Direction& second);
+
 	Direction(__m128 vector);
 
 	__m128 m_Vector = _mm_set_ps(0.f, 0.f, 0.f, 0.f);
 };
+
+Direction operator*(float scale, const Direction& direction);
+float Dot(const Direction& first, const Direction& second);
+// Only calculates a 3D cross product
+Direction Cross(const Direction& first, const Direction& second);
+void GenerateCoordinateSystem(const Direction& normalized, Direction& outFirst, Direction& outSecond);
+Direction Shuffle(const Direction& direction, uint8_t x, uint8_t y, uint8_t z);
+
+// Point in 3D space, with W coordinate of 1
+class Point
+{
+public:
+	Point() = default;
+	Point(float x, float y, float z);
+	Point(const Point& other) = default;
+
+	Point& operator=(const Point& other);
+
+	float operator[](uint8_t index) const;
+	float& operator[](uint8_t index);
+
+	Point operator+(const Direction& direction) const;
+	Point& operator+=(const Direction& direction);
+
+	Direction operator-(const Point& other) const;
+	Point operator-(const Direction& direction) const;
+	Point& operator-=(const Direction& direction);
+
+	bool IsNAN();
+
+	// Whether this should be done or not is debatable
+	float& X = reinterpret_cast<float*>(&m_Vector)[0];
+	float& Y = reinterpret_cast<float*>(&m_Vector)[1];
+	float& Z = reinterpret_cast<float*>(&m_Vector)[2];
+
+private:
+	Point(__m128 vector);
+
+	__m128 m_Vector = _mm_set_ps(1.f, 0.f, 0.f, 0.f);
+};
+
+float Distance(const Point& first, const Point& second);
+float DistanceSquare(const Point& first, const Point& second);
+Point Lerp(const Point& from, const Point& to, float value);
+Point Min(const Point& first, const Point& second);
+Point Max(const Point& first, const Point& second);
+Point Shuffle(const Point& point, uint8_t x, uint8_t y, uint8_t z);
+
+// A surface normal. MAY NOT be normalized
+class Normal
+{
+public:
+	Normal() = default;
+	Normal(float x, float y, float z);
+	Normal(const Normal & other) = default;
+	explicit operator Direction();
+
+	Normal& operator=(const Normal& other);
+
+	float operator[](uint8_t index) const;
+	float& operator[](uint8_t index);
+
+	Normal operator+(const Normal& other) const;
+	Normal& operator+=(const Normal& other);
+
+	Normal operator-() const;
+	Normal operator-(const Normal& other) const;
+	Normal& operator-=(const Normal& other);
+
+	Normal operator*(float scalar) const;
+	Normal& operator*=(float scalar);
+
+	Normal operator/(float scalar) const;
+	Normal& operator/=(float scalar);
+
+	float GetLengthSquare() const;
+	float GetLength() const;
+
+	Normal GetNormalized() const;
+	Normal& Normalize();
+
+	bool IsNAN();
+
+	// Whether this should be done or not is debatable
+	float& X = reinterpret_cast<float*>(&m_Vector)[0];
+	float& Y = reinterpret_cast<float*>(&m_Vector)[1];
+	float& Z = reinterpret_cast<float*>(&m_Vector)[2];
+
+private:
+	friend float Dot(const Normal& first, const Normal& second);
+	friend float Dot(const Normal& first, const Direction& second);
+	friend float Dot(const Direction& first, const Normal& second);
+
+	Normal(__m128 vector);
+
+	__m128 m_Vector = _mm_set_ps(0.f, 0.f, 0.f, 0.f);
+};
+
+float Dot(const Normal& first, const Normal& second);
+float Dot(const Normal& first, const Direction& second);
+float Dot(const Direction& first, const Normal& second);
+Normal FlipAlong(const Normal& normal, const Direction& along);
